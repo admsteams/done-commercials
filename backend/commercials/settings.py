8ee296f979ba94653a2465992f9fa27377262849
admsteams <adms.teams@gmail.com>
2025-10-17 05:10:18 +0530

    revert back

--- backend/commercials/settings.py
+++ backend/commercials/settings.py
@@ -49,7 +49,7 @@ ROOT_URLCONF = 'commercials.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'static')],  # React build copied here
+        'DIRS': [os.path.join(BASE_DIR, 'static/build/')],  # React build copied here
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
